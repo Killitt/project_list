c74a3e15e33ed565c667ed173b27ae26e9a12d99
Ulyana Baranovskaya <baranovskaya128@gmail.com>
2018-12-20 22:04:33 +0300

    database migration for adding models attributes and dependencies

---
+++ db/migrate/20181219202305_add_settings_to_user.rb
@@ -0,0 +1,7 @@
+class AddSettingsToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :first_name, :string, null: false, default: ""
+    add_column :users, :last_name, :string, null: false, default: ""
+    add_column :users, :admin, :boolean, default: false
+  end
+end
